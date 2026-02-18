6befa807a2b5b763009a505c3294bb704e358651
lokeshkumarem-loki <lokeshkumarem@gmail.com>
2026-02-18 15:58:25 +0530

    paosdkf

--- extensions/content-blocker/server.js
+++ extensions/content-blocker/server.js
@@ -1,6 +1,5 @@
-const http = require("http");
-
-const PORT = process.env.PORT || 3000;
+import http from "http";
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
